2821169ce4b077bea422ffa0e637684fe6107f65
Mehmed Muric <mehmedmuric22@gmail.com>
2024-07-06 00:45:08 +0200

    test

--- BigMove/biggestmove.py
+++ BigMove/biggestmove.py
@@ -1,11 +1,19 @@
-import pandas as pd
-import onecall
-import time 
+import pandas as pd #excel for python
+import onecall #handling data
+import time #time
 import schedule
 
+# import file api
+
 size = 1   
+symbol = 'USDCAD'
 
 onecall = onecall.phemex(
     key = 'adadada',
     secret = 'sadadsads',
 )
+
+#orderbook data
+
+ob = onecall.get_orderbook(symbol, True)
+print(ob)
